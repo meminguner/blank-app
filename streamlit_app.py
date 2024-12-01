162d16cdd484551d59aad2b4e2fa02ec4fb1d4d5
meminguner <59503142+meminguner@users.noreply.github.com>
2024-12-01 16:13:00 +0000

    deneme02

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.text_input(value="")
+#st.text_input(value="")
 
 
